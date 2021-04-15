277f6db6feb8bc3f6f513cd3a055cd679dec2e64
Ethan Behrends <epbehrends@gmail.com>
2021-04-15 16:54:25 -0500

    Starting over 😼using Svelte now
    Decided that Svelte would be the best framework to use with this new redesign, very excited to try it out

--- src/Components/Resume.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Resume() {
-  return (<object style={{height: "100vh"}} data="./Assets/Ethan_Behrends_Resume_Fall_2020_Parsable.pdf" type="application/pdf" width="100%" height="100%">
-
-  </object>)
-}
-
-export default Resume
